9960bae1c50b79bb05b6d62ea5ad32eeb529b096
Mads <madeldelacruz@Madss-Mac-mini.local>
2024-12-24 12:13:46 +1300

    added responsiveness

--- src/pages/comingsoon/index.tsx
+++ src/pages/comingsoon/index.tsx
@@ -5,26 +5,27 @@ import FacebookIcon from "@/assets/facebook.svg"
 
 const ComingSoon = () => {
   return (
-    <div className="min-h-screen bg-comingsoon bg-no-repeat bg-cover grid grid-cols-2">
-      <div className="text-white pt-40 mx-40">
+    <div className="min-h-screen bg-comingsoon bg-no-repeat bg-cover lm:bg-center grid grid-rows-2 lg:grid-cols-2">
+      <div className="text-white pt-48 xl:pt-40 mx-8 xs:mx-auto lm:ml-40 ">
         <div>
-          <h1 className="italic text-[5.5rem] tracking-[-0.05em]">Coming Soon</h1>
-          <h2 className="text-[1.7rem] font-comfortaa font-light tracking-[0.01em] -mt-4">Bobby Kurb</h2>
+          <h1 className="italic text-[3rem] xs:text-[4rem] sm:text-[5rem] xl:text-[5.5rem] xs:tracking-[-0.05em]">Coming Soon</h1>
+          <h2 className="text-[1.2rem] sm:text-[1.5rem] xl:text-[1.7rem] font-comfortaa font-light tracking-[0.01em] -mt-2 xs:-mt-4">Bobby Kurb</h2>
         </div>
-        <div className="mt-20">
+        <div className="mt-16 sm:mt-20">
+          <p className="mb-2 text-sm sm:hidden">Notify me when the website is launched</p>
           <div>
             <input
               type="text"
               placeholder="Email"
-              className="p-3 mr-10 w-1/2 opacity-70 text-black"              
+              className="p-3 sm:mr-6 xl:mr-10 w-full sm:w-[22em] xl:w-[25em] opacity-70 text-black"              
             />
-            <button className="bg-white text-black text-sm py-3.5 px-12 rounded-lg transition duration-500 hover:bg-pink-200 hover:text-gray-600">Notify Me</button>
+            <button className="bg-white text-black sm:text-sm py-4 sm:py-3.5  px-16 sm:px-12 rounded-lg transition duration-500 hover:bg-pink-200 hover:text-gray-600 mt-5 sm:mt-0">Notify Me</button>
           </div>
-          <p className="mt-5 text-xs">Notify me when the website is launched</p>
+          <p className="mt-5 text-xs hidden sm:visible">Notify me when the website is launched</p>
         </div>
       </div>
       <div className="flex flex-col-reverse text-white">
-        <div className="py-12 font-light pl-14">
+        <div className="py-12 font-light mx-auto sm:ml-40 lg:pl-24 xxl:pl-14 text-lg">
           <div className="comingsoon-links">
             <img src={EmailIcon} alt="Email icon"/>
             <button>info@bobbykurb.com</button>
